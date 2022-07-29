8c5d59ab4d6d8e2878417ad0ae78820968b27115
MinhPC2k3 <pcminhbg@gmail.com>
2022-07-29 16:39:27 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	//"fmt"
+	"context"
+	"fmt"
+	"time"
+
+	//"math/rand"
+	"sync"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var (
+	channelName = "mychannel"
+	ctx = context.Background()
+)
+
+func InitRedisClient () *redis.Client{
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	return rdb
+}
+func main() {
+	// rdb := redis.NewClient(&redis.Options{
+	// 	Addr:     "localhost:6379",
+	// 	Password: "",
+	// 	DB:       0,
+	// })
+	rdb:=InitRedisClient()
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go sendData(rdb)
+	go printData(rdb)
+	wg.Wait()
+}
+func sendData(rdb *redis.Client) {
+	num := 0
+	for {
+		err := rdb.Publish(ctx, channelName, num).Err()
+		if err != nil {
+			panic(err)
+		}
+		// fmt.Println("process1: ", num)
+		num++
+		time.Sleep(time.Second * 1)
+	}
+}
+func printData(rdb *redis.Client) {
+	sub := rdb.Subscribe(ctx, channelName)
+	c := 0
+	for {
+		message, err := sub.ReceiveMessage(ctx)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(c, message)
+		c++
+	}
+}
